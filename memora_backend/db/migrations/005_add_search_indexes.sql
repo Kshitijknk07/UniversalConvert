7728d5dff940094792ac6ac4b9ecf56110075d24
Kshitijknk07 <knk200407@gmail.com>
2025-05-01 10:17:27 +0530

    New Idea

--- memora_backend/db/migrations/005_add_search_indexes.sql
+++
@@ -1,3 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_dreams_title_description ON dreams USING gin (to_tsvector('english', title || ' ' || description));
-CREATE INDEX IF NOT EXISTS idx_dreams_type ON dreams(dream_type);
-CREATE INDEX IF NOT EXISTS idx_dreams_date ON dreams(dream_date);
